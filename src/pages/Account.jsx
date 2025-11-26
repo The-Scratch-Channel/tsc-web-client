ce6265f339658074dfb73cdee9945d6b53a07678
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-11-26 19:16:06 +0000

    Use username instead of email
    Signed-off-by: 6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -32,7 +32,7 @@ export default function Account() {
 			{user ? (
 				<>
 					<p>
-						{t("account.welcome")}, {user.email}!
+						{t("account.welcome")}, {user.displayName || user.email}!
 					</p>
 					<button onClick={handleLogout}>
 						{t("account.logout")}
