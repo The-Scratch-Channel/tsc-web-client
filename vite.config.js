1afedb372ee9742cc12fb3b0f6af5492911cf7da
Graison P <119984713+Graison-P@users.noreply.github.com>
2025-12-01 08:39:55 -0600

    is this right idk help me God I wanna make a backup
    Signed-off-by: Graison P <119984713+Graison-P@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+	plugins: [react()],
+	base: '/tscbackup/',
+
 	server: {
 		port: 3000,
 		open: true,
