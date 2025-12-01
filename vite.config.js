182ece169eca2ecdb31bd073baf90be0c4e54693
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-01 16:49:01 +0000

    Fix error
    Signed-off-by: 6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	base: '/tscbackup/',
-
 	server: {
 		port: 3000,
 		open: true,
